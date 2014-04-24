079fe4e947c6cd62454e8948d1d953679709e286
Gabriel Dumitrescu <dumigab77@gmail.com>
2014-04-24 22:31:48 +0300

    Start Technique class tutorial 17

---
+++ technique.h
@@ -0,0 +1,36 @@
+/* 
+ * File:   technique.h
+ * Author: quentin
+ *
+ * Created on April 24, 2014, 8:38 PM
+ */
+
+#ifndef TECHNIQUE_H
+#define	TECHNIQUE_H
+
+
+#include <list>
+#define INVALID_UNIFORM_LOCATION 0xFFFFFFFF
+
+class Technique{
+public:
+    Technique();
+    virtual ~Technique();
+    virtual bool Init();
+    void Enable();
+protected:
+    bool AddShader(GLenum shaderType,const char* fileName);
+    bool Finalize();
+    GLint GetUniformLocation(const char* uniformName);
+    GLint GetProgramParam(GLint param);
+    GLuint shaderProgram;
+public:
+    typedef std::list<GLuint> ShaderObjList;
+    ShaderObjList shaderObjectList;
+    
+};
+
+
+
+#endif	/* TECHNIQUE_H */
+
