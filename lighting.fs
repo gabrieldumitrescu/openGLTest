217e5b79f9d57f6be156e2417c43e0df4e88428d
Gabriel Dumitrescu <dumigab77@gmail.com>
2014-05-26 15:43:54 +0300

    Implemented specular lighting. (Tutorial19)

--- lighting.fs
+++ lighting.fs
@@ -3,6 +3,7 @@
 in vec2 TexCoord0;
 
 in vec3 Normal0;
+in vec3 WorldPos0;
 
 out vec4 FragColor;
 
@@ -16,19 +17,36 @@ struct DirectionalLight{
 };
 
 uniform DirectionalLight gDirectionalLight;
+uniform vec3 gEyeWorldPos;
+uniform float gMatSpecularIntensity;
+uniform float gMatSpecularPower;
 
 void main()
 {
     vec4 AmbientColor=vec4(gDirectionalLight.Color,1.0f)*
                 gDirectionalLight.AmbientIntensity;
-    float DiffuseFactor=dot(normalize(Normal0),-gDirectionalLight.Direction);
-    vec4 DiffuseColor;
+
+    vec3 LightDirection=-gDirectionalLight.Direction;
+    vec3 Normal=normalize(Normal0);
+
+    float DiffuseFactor=dot(Normal,LightDirection);
+
+    vec4 DiffuseColor=vec4(0.0,0.0,0.0,0.0);
+    vec4 SpecularColor=vec4(0.0,0.0,0.0,0.0);
+
     if(DiffuseFactor>0){
         DiffuseColor=vec4(gDirectionalLight.Color,1.0) * 
                     gDirectionalLight.DiffuseIntensity * DiffuseFactor;
+        vec3 VertexToEye=normalize(gEyeWorldPos-WorldPos0);
+        vec3 LightReflect=normalize(reflect(gDirectionalLight.Direction, Normal));
+        float SpecularFactor=dot(VertexToEye, LightReflect);
+        SpecularFactor=pow(SpecularFactor,gMatSpecularPower);
+        if(SpecularFactor > 0){
+            SpecularColor=vec4(gDirectionalLight.Color,1.0) * gMatSpecularIntensity *
+                          SpecularFactor;
+        }
+
     }
-    else{
-        DiffuseColor=vec4(0.0,0.0,0.0,0.0);
-    }
-    FragColor = texture2D(gSampler, TexCoord0.xy)*(AmbientColor + DiffuseColor);
+    
+    FragColor = texture2D(gSampler, TexCoord0.xy)*(AmbientColor + DiffuseColor+SpecularColor);
 }
